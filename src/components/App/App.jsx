c9bbd50583a22ed06c1bd6b7c6c8373347dd13ec
SergioUAX <itserver.ua@gmail.com>
2025-05-13 14:28:51 +0200

    prepare3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,5 @@
 import styles from './App.module.css';
-// import ContactList from '../ContactList/ContactList';
-// import ContactForm from '../ContactForm/ContactForm';
-// import SearchBox from '../SearchBox/SearchBox';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchContacts } from '../../redux/contactsOps';
 import { Routes, Route } from 'react-router-dom';
-import Navigation from '../Navigation/Navigation';
 import Home from '../../pages/Home/Home';
 import Login from '../../pages/Login/Login';
 import Register from '../../pages/Register/Register';
@@ -14,12 +7,7 @@ import Contacts from '../../pages/Contacts/Contacts';
 import NotFound from '../../pages/NotFound/NotFound';
 import SharedLayout from '../SharedLayout/SharedLayout';
 
-function App() { 
-  const dispatch = useDispatch();
-  useEffect(() => { 
-    dispatch(fetchContacts());
-  },[dispatch]);
-
+function App() {   
   return (
     <>      
         <Routes>
--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,8 +1,17 @@
 import styles from './contacts.module.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contactsOps';
+
 import ContactForm from '../../components/ContactForm/ContactForm';
 import ContactList from '../../components/ContactList/ContactList';
 import SearchBox from '../../components/SearchBox/SearchBox';
 
+const dispatch = useDispatch();
+  useEffect(() => { 
+    dispatch(fetchContacts());
+  }, [dispatch]);
+    
 const Contacts = () => {
     return (
         <>
